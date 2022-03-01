a57948ccf08304a179de2b449f87e61f17108643
Gabriel Regis <86062383+Regis2077@users.noreply.github.com>
2022-02-28 23:08:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -60,22 +60,3 @@ function btncopiar() {
 
 
 
-/*Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação   
-*/
-
-/* Regras Decodificador: 
-"enter" é convertido para "e" 
-"imes" é convertido para "i"
-"ai" é convertido para "a"
-"ober" é convertido para "o"
-"ufat" é convertido para "u"
-Apenas letras minúsculas
-Não permite acentuação     
-*/
